6d95f66acc750d3019fddbed7af3674e4aede4b6
AndroidOS <burrowsgit@gmail.com>
2020-11-19 07:14:59 +1100

    Added readFile

--- cypher/main.swift
+++ cypher/main.swift
@@ -6,6 +6,24 @@
 //
 
 import Foundation
+import Cocoa
 
 print("Hello, World!")
+
+print(readFile())
+
+
+func readFile() -> String {
+   var text = ""
+    do {
+        text =  try String(contentsOfFile:"/Users/manuelcarvalho/Documents/cypher.txt")
+    } catch {
+        print("Unexpected error: \(error).")
+    }
+    return text
+}
+
+
+
+
 
