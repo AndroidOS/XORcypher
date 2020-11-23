b19c62156b6dbf27a689538ee9d86d1bafe86fd3
AndroidOS <burrowsgit@gmail.com>
2020-11-24 07:12:06 +1100

    Added encrypting() skeleton

--- cypher/main.swift
+++ cypher/main.swift
@@ -8,28 +8,39 @@
 import Foundation
 import Cocoa
 
-//let key = [101,120,112]
 
+decrypting()
 
-
-let str = readFile()
-let nums = splitString(str: str)
-
-for a in 97...122 {
-    for b in 97...122 {
-        for c in 97...122 {
-            
-            print("key: \(a) \(b) \(c)")
-            let clear_text = decypher(num: nums, key: [a,b,c])
-            if englishTest(str: clear_text){
-                print(clear_text)
-            }
-        }
-    }
+func encrypting(){
+    let str = readFile(str: "cypher.txt")
     
 }
 
-//print(nums)
+
+func decrypting(){
+    
+    let str = readFile(str: "cypher.txt")
+    let nums = splitString(str: str)
+
+    print("Decrypting ....\n")
+
+    for a in 97...122 {
+        for b in 97...122 {
+            for c in 97...122 {
+            
+            
+                let clear_text = decypher(num: nums, key: [a,b,c])
+                if englishTest(str: clear_text){
+                    print(clear_text)
+                    print("key: \(a) \(b) \(c)")
+                }
+            }
+        }
+        
+    }
+}
+
+print("\n\nEnd decrypting")
 
 func englishTest(str: String) -> Bool{
     
@@ -71,10 +82,10 @@ func splitString(str:String) -> [Int] {
 }
 
 
-func readFile() -> String {
+func readFile(str: String) -> String {
    var text = ""
     do {
-        text =  try String(contentsOfFile:"/Users/manuelcarvalho/Documents/cypher.txt")
+        text =  try String(contentsOfFile:"/Users/manuelcarvalho/Documents/" + str)
     } catch {
         print("Unexpected error: \(error).")
     }
